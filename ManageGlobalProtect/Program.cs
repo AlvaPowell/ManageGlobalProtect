b92736e428e70608f25cf9489253d1c91a931a41
alva <alva@GIZMO>
2021-06-18 14:52:42 +0530

    Initial version

---
+++ ManageGlobalProtect/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.ServiceProcess;
+
+namespace ManageGlobalProtect
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ServiceController controller = new ServiceController("PanGPS");
+
+
+
+            if (controller.Status == ServiceControllerStatus.Stopped)
+            {
+                controller.Start();
+                controller.WaitForStatus(ServiceControllerStatus.StartPending);
+            }
+            else
+            {
+                controller.Stop();
+                controller.WaitForStatus(ServiceControllerStatus.Stopped); 
+            }
+
+            Console.WriteLine("Service status: " + controller.Status);
+            Console.ReadLine();
+        }
+    }
+}
